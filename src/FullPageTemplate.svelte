48ef52aa79dc5e143a30318c1051af2da271be0b
marclevetin <marc@derbyhat.org>
2020-05-24 19:09:26 -0400

    added quick comment about full page transition

--- src/FullPageTemplate.svelte
+++ src/FullPageTemplate.svelte
@@ -8,6 +8,7 @@
 </style>
 
 <!-- Fade is not working -->
+<!-- See https://github.com/EmilTholin/svelte-routing/issues/66 -->
 <main transition:fade="{{ duration: 2000 }}">
     <slot>Something went wrong</slot>
 </main>
